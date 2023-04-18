749ca02170ec7157aeec2434865aabdaf1373b9e
JoseC620 <Jcepeda6201@gmail.com>
2023-04-18 17:45:15 -0400

    added true and false for withdrawl and deposit values

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const logger = require("morgan")
 const bankStatementsController = require("./controllers/bankStatementsController.js")
 
-
-app.use(cors());
 app.use(express.json())
+app.use(cors());
+
+app.use(logger('dev'))
 app.use("/statements", bankStatementsController)
 
 app.get(("/"), ( req, res ) => {
--- models/bankStatements.js
+++ models/bankStatements.js
@@ -2,33 +2,41 @@ module.exports = [
     {
         id: "0",
         item_name: "Direct Deposit",
-        amount: "$5000",
+        amount: 5000,
         date: "April 1",
         from: "Coding Job",
-        category: "Income"
+        category: "Income",
+        withdrawl: false,
+        deposit: true
     },
     {
         id: "1",
         item_name: "Exxon gas",
-        amount: "-$35",
+        amount: 35,
         date: "April 1",
         from: "Gas Station",
-        category: "Personal Spending"
+        category: "Personal Spending",
+        withdrawl: true,
+        deposit: false
     },
     {
         id: "2",
         item_name: "Ruth's Chris Steak House",
-        amount: "-$500",
+        amount: 500,
         date: "April 1",
         from: "Restaurant",
-        category: "Food"
+        category: "Food",
+        withdrawl: true,
+        deposit: false
     },
     {
         id: "3",
         item_name: "Savings Transfer",
-        amount: "-$3500",
+        amount: 3500,
         date: "April 1",
         from: "Checkings Account",
-        category: "Transfer"
+        category: "Transfer",
+        withdrawl: true,
+        deposit: false
     }
 ]
